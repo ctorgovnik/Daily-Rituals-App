cb6fccd6f10a23d4e3f62eda929f7fc1d3490555
Cody Torgovnik <cjt76@cornell.edu>
2022-12-16 21:36:32 -0500

    Initial Commit

---
+++ SwiftNavigation/SwiftNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNavigationApp.swift
+//  SwiftNavigation
+//
+//  Created by Cody Torgovnik on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
